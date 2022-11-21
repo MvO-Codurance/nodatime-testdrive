39a9bfd8847734e90fc74a213b69ea75a08ef2b8
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-21 11:29:43 +0000

    Added test to ensure that all TimezonesForDisplay are included in AllTimezones".

--- NodaTimeTestDrive.Tests/ClockServiceShould.cs
+++ NodaTimeTestDrive.Tests/ClockServiceShould.cs
@@ -31,4 +31,17 @@ public class ClockServiceShould
         timezones[138].Id.Should().Be("Pacific/Kiritimati");
         timezones[138].Name.Should().Be("(UTC+14:00) Kiritimati Island");
     }
+    
+    [Fact]
+    public void Ensure_Ids_From_TimezonesForDisplay_Are_A_Subset_Of_AllTimezones()
+    {
+        var clock = new FakeClock(Instant.FromUtc(2022, 11, 20, 10, 30));
+        var timezoneProvider = DateTimeZoneProviders.Tzdb;
+        var sut = new ClockService(clock, timezoneProvider);
+
+        var allTimezoneIds = sut.AllTimezones.Select(x => x.Id).ToList();
+        var timezoneIdsForDisplay = sut.TimezonesForDisplay.Select(x => x.Id).ToList();
+
+        timezoneIdsForDisplay.Except(allTimezoneIds).Should().HaveCount(0);
+    }
 }
