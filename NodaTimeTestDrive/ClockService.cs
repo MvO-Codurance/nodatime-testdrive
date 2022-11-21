18d63896629595c49aa6a8169080e1da2e5e20d0
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-21 11:52:19 +0000

    Added more explanatory comments around the need for and use of TimezonesForDisplay.

--- NodaTimeTestDrive/ClockService.cs
+++ NodaTimeTestDrive/ClockService.cs
@@ -9,7 +9,7 @@ public class ClockService : IClockService
 {
     private readonly IClock _clock;
     private readonly IDateTimeZoneProvider _timezoneProvider;
-    
+
     public DateTimeZone TimeZone => throw new NotImplementedException();
     public Instant Now => throw new NotImplementedException();
     public LocalDateTime LocalNow => throw new NotImplementedException();
@@ -17,7 +17,14 @@ public class ClockService : IClockService
     // This returns all NodaTime (IANA) timezones but the main useful property is the actual Id
     public IEnumerable<DateTimeZone> AllTimezones => _timezoneProvider.GetAllZones();
     
-    // This returns timezones that are suitable for display (using the Name property), mapped back to the IANA Id. 
+    /* This returns timezones that are suitable for display (using the Name property), mapped back to the IANA Id.
+     * This is required to have a better UX for end-users.
+     * As NodaTime doesn't include support for generating this list (see https://stackoverflow.com/a/61496018)
+     * we use the "TimeZoneNames" NuGet package instead (see https://stackoverflow.com/a/62048885).
+     * However, also note that whilst the "TimeZoneNames" NuGet package suggests it is no longer needed for .NET 6.0+
+     * (see https://github.com/mattjohnsonpint/TimeZoneNames) this still leads to inconsistency between Windows and
+     * Linux timezone ids. So, we use "TimeZoneNames" to ensure consistent use of the IANA timezone ids.
+     */
     public IEnumerable<TimezoneForDisplay> TimezonesForDisplay {
         get
         {
@@ -27,7 +34,7 @@ public class ClockService : IClockService
                 .OrderBy(x => x.Name);    
         }
     }
-
+    
     public ClockService(IClock clock, IDateTimeZoneProvider timezoneProvider)
     {
         _clock = clock;
